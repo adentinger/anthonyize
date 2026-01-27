38ad42b65d67b045bbd8046c4b45fc139a95d6ab
Anthony Dentinger <anthony.dentinger@spacecodesign.com>
2026-01-27 18:03:19 -0500

    Tweak comments in powershell script

--- scripts/services-setup-runas.ps1
+++ scripts/services-setup-runas.ps1
@@ -1,10 +1,11 @@
 # A script meant to be run as admin
 
 # Re-run this script as admin if not already running as admin. Apparently
-# doesn't keep the current working directory though. Added "-Wait" and
-# to ensure the elevated process completes before exiting this one, and
-# "-ExecutionPolicy Bypass" to be allowed to run this script.
+# doesn't keep the current working directory though.
 # https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html
+# Compared with the above link, added "-Wait" and to ensure the elevated
+# process completes before exiting this one, and "-ExecutionPolicy Bypass" to
+# be allowed to run this script.
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
         $CommandLine = "-ExecutionPolicy Bypass -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
